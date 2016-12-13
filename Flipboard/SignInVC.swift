37e5e3f2c313e0b952a4b29966e9fb3ef81590bd
Miguel Santos <miggsantos@gmail.com>
2016-12-13 23:50:47 +0000

    Add SignIn and SignUp View Controllers

---
+++ Flipboard/SignInVC.swift
@@ -0,0 +1,43 @@
+//
+//  SignInVC.swift
+//  Flipboard
+//
+//  Created by Miguel Santos on 04/12/2016.
+//  Copyright © 2016 Miguel Santos. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    
+    @IBAction func CancelPressed(_ sender: Any) {
+    
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
